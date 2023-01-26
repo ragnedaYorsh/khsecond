ab64730a0bbfa887370f0395eea49d70e808e1d6
ragneda <ragnedapython@gmail.com>
2023-01-27 00:24:42 +0300

    postgres

--- khsecond/khsecond/settings.py
+++ khsecond/khsecond/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'khsecond.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'second_db',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'secondkh',
+        'USER': 'yorsh',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
