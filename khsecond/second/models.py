0df82ee389c7b6eef1df6d5ccff12d4d59422ee6
ragneda <ragnedapython@gmail.com>
2023-02-01 12:18:19 +0300

    photo

--- khsecond/second/models.py
+++ khsecond/second/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Thing(models.Model):
+    type = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
+    size = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    price = models.IntegerField()
+    material = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
+
+
+class User(models.Model):
+    things = models.ManyToManyField(Thing)
